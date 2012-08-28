5798ef260f162c6265c069e8a9d52dd5d5581296
Chris McGinlay <chris@unpossible.info>
2012-08-28 23:55:00 +0100

    Model docstrings tidied up and added attribute descriptions

--- courses/models.py
+++ courses/models.py
@@ -6,108 +6,139 @@ from django.db.models.signals import post_save
 #TODO implement get_absolute_url() methods
 
 class Course(models.Model):
-    #abbreviated course code
+    """A self contained course of study.
+    
+    Attributes:
+        course_code         Course code for human consumption
+        course_name         Human readable name of course
+        course_abstract     Summary paragraph outlining course
+        course_organiser    Person responsible for this course
+        course_level        Level of difficulty of this course (eg SCQF)
+        course_credits      Number of points awarded on completion
+       
+    """
+
     course_code = models.CharField(max_length=10)
-
-    #full name of course
     course_name = models.CharField(max_length=150)
-    
-    #summary paragraph of course
     course_abstract = models.TextField()
-
-    #name of person responsible for course
+    
+    #TODO change this to ForeignKey(UserPrf)
     course_organiser = models.CharField(max_length=100)
-
-    #some scheme of level of advancement/difficulty
     course_level = models.CharField(max_length=10, blank=True, null=True)
-    
-    #number of credit points awarded on completion
     course_credits = models.IntegerField(blank=True, null=True)
-    
-    #attachments held in Foreign Key with attachment class
-    #intro video held in Video class
     
     def __unicode__(self):
         return self.course_name
         
+        
 class Lesson(models.Model):
-    #Human readable abbreviated lesson name
+    """A digestible chunk of learning material within a course.
+    
+    Attributes:
+        lesson_code     Lesson code for human consumption
+        lesson_name     Human readable full lesson name
+        course          ForeignKey - course to which lesson belongs
+        abstract        short text description of the lesson
+        
+    """
+    
     lesson_code = models.CharField(max_length=10, blank=True, null=True)
     lesson_name = models.CharField(max_length=200)
-    
-    #course to which lesson belongs
     course = models.ForeignKey(Course)
-    
-    #short text description of the lesson
     abstract = models.TextField()
 
     def get_next(self):
-        '''Return the next lesson in the course'''
+        """Return the next lesson in the course"""
         next = Lesson.objects.filter(course=self.course).filter(id__gt=self.id)
         if next:
             return next[0]
         return False
         
     def get_prev(self):
-        '''Return the previous lesson in the course'''
+        """Return the previous lesson in the course"""
         prev = Lesson.objects.filter(course=self.course).filter(id__lt=self.id).order_by('-id')
         if prev:
             return prev[0]
         return False
-        
-    def get_id(self):
-        '''For debug purposes'''
-        return self.id
     
     def __unicode__(self):
         return self.lesson_name
         
 class Video(models.Model):
-    #TODO: use YouTube / Vimeo APIs to pull video meta data such as title, tags    
+    """Details of video resources used in lessons.
     
-    #Human readable abbreviated video name
+    Note that videos may have foreign keys directly to a course, not just 
+    to lessons. This allows for introductory videos to belong to whole course
+        
+    Attributes:
+        video_code  Video code for human consumption
+        video_name  Full human readable name of video
+        url         url of the video resource
+        lesson      ForeignKey - if the video is embedded in a lesson
+        course      ForeignKey - if the video is embedded directly in course
+    
+    """
+    
+    #TODO: use YouTube / Vimeo APIs to pull video meta data such as title, tags    
     video_code = models.CharField(max_length=10, blank=True, null=True)
     video_name = models.CharField(max_length=200)
     url = models.URLField()
-    
-    #video may belong to lesson or directly to course.
+    #TODO override __init__ to ensure precisely one of these is not null
     lesson = models.ForeignKey(Lesson, blank=True, null=True)
     course = models.ForeignKey(Course, blank=True, null=True)
     
     def __unicode__(self):
         return self.video_name
         
+        
 class Attachments(models.Model):
-    '''Handle all forms of attachment except video'''
+    """Handle all forms of attachment except video.
+    
+    Attributes:
+        att_code    Attachment code for human consumption
+        att_name    Human readable name of the attachment
+        lesson      ForeignKey - if attachment embedded in lesson
+        course      ForeignKey - if attachment embedded in course
+        att_desc    Paragraph describing contents of attachment
+        att_seq     Optional unique sequence number of attachment
+        att_url     URL to file (local/external)
+
+    """
+    
     #TODO: better modules exist for this.
     #TODO: remove plural from this class name, not consistent!
     
-    #Human readable abbreviated name and name
     att_code = models.CharField(max_length=10, blank=True, null=True)
     att_name = models.CharField(max_length=200)
-    
-    #attachment may belong to a lesson or directly to course
-    #TODO insist precisely one of the following is valid.
+    #TODO: override __init__ to ensure precisely one of the following is not null
     lesson = models.ForeignKey(Lesson, blank=True, null=True)
     course = models.ForeignKey(Course, blank=True, null=True)
-    
-    #Optional description of attachment
     att_desc = models.TextField(blank=True, null=True)
-    #Optional sequence number of attachment
     att_seq = models.IntegerField(blank=True, null=True)
-    
-    #local filesystem URL
     att_url = models.URLField()
     
+    class Meta:
+        unique_together = (("lesson", "att_seq"),)   
+        
     def __unicode__(self):
         return self.att_name
         
-#Based on section 3.10 of PDF manual
+
 class UserProfile(models.Model):
-    '''Extend User module with additional data'''
-    #required field
-    user = models.OneToOneField(User)
+    """Extend User module with additional data.
     
+    Custom user profile data based on section 3.10 of PDF manual
+    
+    Attributes:
+        user                1-1Field - Required field - User
+        lessons             Intermediary - user interaction with lessons
+        accepted_terms      Either you do or you don't
+        signature_line      Personal motto for user
+        registered_courses  m-mField - Courses user is registered on
+    
+    """
+    
+    user = models.OneToOneField(User)
     lessons = models.ManyToManyField(Lesson, through='UserProfile_Lesson')
     accepted_terms = models.BooleanField()
     signature_line = models.CharField(max_length=200)
@@ -116,16 +147,24 @@ class UserProfile(models.Model):
     def __unicode__(self):
         return self.user.username
         
-# extraneous: implemented in User class
-#    def __unicode__(self):
-#       return self.user.username
 
 #########################################
 #Table join classes, i.e. intermediary models
 #########################################
 
 class UserProfile_Lesson(models.Model):
-    '''Store data relating to a user's interaction with a lesson'''
+    """Store data relating to a user's interaction with a lesson.
+    
+    Attributes:
+        userprofile     ForeignKey - the django user object
+        lesson          ForeignKey - a lesson the user interacted with
+        mark_complete   True if this lesson has been 'completed' in some way
+        date_complete   Date on which lesson marked complete.
+        
+    """
+    
+#TODO create a method to set date_complete automatically when mark_complete 
+#is set True.
     userprofile = models.ForeignKey(UserProfile)
     lesson = models.ForeignKey(Lesson)
     mark_complete = models.BooleanField(default=False)
@@ -141,9 +180,6 @@ class UserProfile_Lesson(models.Model):
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-    '''Ensure user profile is created for each new user'''
+    """Ensure user profile is created for each new user"""
     if created:
         UserProfile.objects.create(user=instance)
-
-#Register to receive the signal
-#post_save.connect(create_user_profile, sender=User)
--- quiz/models.py
+++ quiz/models.py
@@ -5,11 +5,14 @@ from courses.models import Lesson
 
 # TODO eventually create other types of quiz, using an abstract base class
 # Early days: get everything working with simple multichoice.
-
 class Answer(models.Model):
     """Multichoice style answer. 
     
     Represent a possible answer along with explanatory text
+    
+    Attributes:
+        answer_text     Human readable text of possible answer
+        explan_text     Explanation of why this answer is right/wrong
     
     """
     
@@ -24,8 +27,17 @@ class Answer(models.Model):
 # Should be unique to a quiz and question numbers should be contiguous
 # within each quiz.
 class Question(models.Model):
-    """Multichoice question"""
+    """Multichoice question.
     
+    Attributes:
+        question_text   Human readable text of the question
+        answers         m-mField - all contender answers
+        correct_answer  ForeignKey - the 'correct' answer
+        
+    """
+    
+#TODO ensure that answers includes correct_answer, possibly could do this
+#when the question is included in a quiz
     question_text = models.TextField()
     answers = models.ManyToManyField(Answer)
     correct_answer = models.ForeignKey(Answer, related_name="valid")
@@ -35,8 +47,16 @@ class Question(models.Model):
 
         
 class Quiz(models.Model):
-    """A multi-choice quiz"""
+    """A multi-choice quiz.
     
+    Attributes:
+        quiz_title  Human readable name of the quiz
+        lesson      ForeignKey - lesson to which quiz belongs
+        create_date Date on which quiz was first created
+        author      ForeignKey - author user
+        questions   m-mField - all questions constituting this course
+    
+    """
     quiz_title = models.CharField(max_length=200)
     lesson = models.ForeignKey(Lesson)
     create_date = models.DateField(auto_now=True)
@@ -51,7 +71,21 @@ class Quiz(models.Model):
         return self.quiz_title
 
 class Attempt(models.Model):
-    """Responses made by user during an attempt of the quiz"""
+    """Responses made by user during an attempt of the quiz.attname
+    
+    Note that we DO need to record the question object here too, since we 
+    will record the user's response to each question in the database, and we
+    will score each response separately.
+    
+    Attributes:
+        user            ForeignKey - who is attempting the quiz
+        quiz            ForeignKey - the quiz
+        taken_dt        Date quiz first taken
+        question        ForeignKey - a question of the quiz
+        answer_given    The answer object the user thinks to be 'correct'
+        score           Points allocated to answer_given
+    
+    """
     
     user = models.ForeignKey(User)
     quiz = models.ForeignKey(Quiz)
@@ -59,8 +93,8 @@ class Attempt(models.Model):
     question = models.ForeignKey(Question)
     answer_given = models.ForeignKey(Answer)
     score = models.IntegerField(default=0)
-    
-    #TODO intercept signal on attempt create/alter to calculate score
+#TODO intercept signal on attempt create/alter to calculate score or maybe 
+#just do in form
 
     def __unicode__(self):
         return "User: %s, Quiz: %s" % (self.user, self.quiz)
