9c02696f769e7ccd1c17cf4214678ea333455965
Chris McGinlay <chris@unpossible.info>
2013-01-18 23:11:37 +0000

    More model tests. 404 and 500 error templates

--- courses/tests.py
+++ courses/tests.py
@@ -3,10 +3,10 @@ Unit tests for Courses app
 """
 
 from django.test import TestCase
-from courses.models import Course
+from courses.models import Course, Lesson, Video, Attachment
 
 class CourseModelTests(TestCase):
-    """Test the model used to represent courses"""
+    """Test the models used to represent courses and constituent lessons etc"""
 
 #TODO load data from JSON fixtures if these instances become irksome
     course1_data = {'course_code': 'EDU02',
@@ -22,19 +22,68 @@ class CourseModelTests(TestCase):
                    'course_organiser': 'Lee Marvin',
                    'course_level': 5,
                    'course_credits': -20,
-                   }                 
+                   }
+    lesson1_data = {'lesson_code': 'B1',
+                    'lesson_name': 'Introduction to Music',
+                    'abstract': 'A summary of what we cover',
+                   }
+    video1_data = {'video_code': 'MV2',
+                   'url': 'http://youtu.be/LIM--jfnKeU',
+                   'video_name': 'Music introduction',
+                  }
+    attachment1_data = {'att_code': 'DOC1',
+                        'att_name': 'Reading List',
+                        'att_desc': 'Useful stuff you might need',
+                        'att_seq': 3,
+                        }
     
     def setUp(self):
         self.course1 = Course(**self.course1_data)
         self.course1.save()
         self.course2 = Course(**self.course2_data)
         self.course2.save()
+        self.lesson1 = Lesson(course=self.course1, **self.lesson1_data)
+        self.lesson1.save()
+        self.video1 = Video(course=self.course1, **self.video1_data)
+        self.video1.save()
+        self.video2 = Video(lesson=self.lesson1, **self.video1_data)
+        self.video2.save()
+        self.attachment1 = Attachment(course=self.course1, 
+                                      **self.attachment1_data)
+        self.attachment1.save()
         
     def test_course_create(self):
         """All instance attributes are created OK"""
         for key,val in self.course1_data.items():
             self.assertEqual(self.course1.__dict__[key], val)
+    
+    def test_lesson_create(self):
+        for key,val in self.lesson1_data.items():
+            self.assertEqual(self.lesson1.__dict__[key], val)
+        self.assertEqual(self.lesson1.course, self.course1)
+            
+    def test_video_create(self):
+        #Associated with course
+        for key,val in self.video1_data.items():
+            self.assertEqual(self.video1.__dict__[key], val)
+        self.assertEqual(self.video1.course, self.course1)
+            
+        #Associated with lesson
+        for key,val in self.video1_data.items():
+            self.assertEqual(self.video2.__dict__[key], val)
+        self.assertEqual(self.video2.lesson, self.lesson1)
+            
+    def test_attachment_create(self):
+        for key,val in self.attachment1_data.items():
+            self.assertEqual(self.attachment1.__dict__[key], val)   
+        self.assertEqual(self.attachment1.course, self.course1)
 
+    def test_userProfile_create(self):
+        assert (False)  #stub
+    
+    def test_userProfile_Lesson_create(self):
+        assert (False)  #stub
+    
 class CourseViewTests(TestCase):
     """Test the course views"""
     
