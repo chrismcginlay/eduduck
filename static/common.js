726dd9fd2f1d130cfc06ce1a4230fb402aac291d
Chris McGinlay <chris@unpossible.info>
2013-01-30 01:35:26 +0000

    Closes #7. Will need further visual enhancement later using JQueryUI.

---
+++ static/common.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){    
+    $( ".FYI" ).click(function(){
+        $( this ).toggle( "drop" );
+    });
+    $( ".abstract" ).click(function(){
+        $( this ).toggle( "drop" );
+    });
+});
