4aff4012be3bf263a1a1546ddc8e273e5af50d00
Chris McGinlay <chris@unpossible.info>
2013-03-09 15:49:37 +0000

    Closes #28

--- interaction/admin.py
+++ interaction/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
-from interaction.models import UserCourse
+from .models import UserCourse
 
-admin.site.register(UserCourse)
+class UserCourseAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'course', 'active')
+    list_filter = ('user', 'course')
+    search_fields = ('user', 'course')
+    fields = (('user', 'course'), 'active', 'completed', 'withdrawn', 'history')
+    readonly_fields = ('user', 'course', 'active', 'completed', 'withdrawn', 'history')
+
+admin.site.register(UserCourse, UserCourseAdmin)
