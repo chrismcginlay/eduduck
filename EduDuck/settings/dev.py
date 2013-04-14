22c19071cc8f18cad3f509c0edda6fdbbef5513a
chrismcginlay <ctmcginlay@gmail.com>
2013-04-14 15:42:11 +0100

    Commented out debug_toolbar meantime

--- EduDuck/settings/dev.py
+++ EduDuck/settings/dev.py
@@ -9,9 +9,9 @@ TEMPLATE_STRING_IF_INVALID = 'INVALID_EXPRESSION: %s'
 #smtp is the default, so in prod.py, EMAIL_BACKEND is commented out or missing
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-INSTALLED_APPS += ("debug_toolbar", )
-INTERNAL_IPS = ("127.0.0.1", )
-MIDDLEWARE_CLASSES += ("debug_toolbar.middleware.DebugToolbarMiddleware",)
+#INSTALLED_APPS += ("debug_toolbar", )
+#INTERNAL_IPS = ("127.0.0.1", )
+#MIDDLEWARE_CLASSES += ("debug_toolbar.middleware.DebugToolbarMiddleware",)
 
 DATABASES = {
     'default': {
