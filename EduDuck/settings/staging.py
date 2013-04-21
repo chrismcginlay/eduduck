952b1a3e0979d429be654ec9b965f7759bf31e15
chrismcginlay <ctmcginlay@gmail.com>
2013-04-21 01:54:16 +0100

    Initial commit for #42. Breaks stuff.

--- EduDuck/settings/staging.py
+++ EduDuck/settings/staging.py
@@ -2,13 +2,24 @@
 from base import *
 import os
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
-TEMPLATE_STRING_IF_INVALID = 'INVALID_EXPRESSION: %s'
+#TEMPLATE_STRING_IF_INVALID = 'INVALID_EXPRESSION: %s'
+TEMPLATE_STRING_IF_INVALID = 'TEMPLATE_ERROR'   #don't expose var names
 
+ALLOWED_HOSTS = ['www.eduduck.com']
+
+STATIC_URL = '/static/'
 #django-registration needs an MTA. For development just use console
-#smtp is the default, so in prod.py, EMAIL_BACKEND is commented out or missing
+#smtp is the default, so for production, use default EMAIL_BACKEND 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+#Fill in for given MTA
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = 'bob'
+EMAIL_HOST_PASSWORD = 'bobo'
+EMAIL_USE_TLS = 'True'
 
 # Make SECRET_KEY unique, and don't share it with anybody.
 # see issue #43 for key generation method.
