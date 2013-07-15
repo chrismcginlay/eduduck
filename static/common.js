d8329dfced89cd455dd2be7a666c1fcf6494555e
Chris McGinlay <ctmcginlay@gmail.com>
2013-07-15 22:17:55 +0100

    Far more sensible jQuery animations.
    Offers shade up/down using &darr; &uarr; HTML entities. Some attention to positioning needed

--- static/common.js
+++ static/common.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){    
-    $( ".FYI" ).click(function(){
-        $( this ).toggle( "drop" );
-    });
-    $( ".abstract" ).click(function(){
-        $( this ).toggle( "drop" );
+    $( '<span class="shade" title="Show/Hide">&uarr;</span>' ).insertBefore( ".abstract" );
+    $( '<span class="shade" title="Show/Hide">&uarr;</span>' ).insertBefore( ".FYI" );
+
+    $( ".shade" ).click(function(){
+        $( this ).next().slideToggle( "slow" );
+        //http://www.unicode.org/charts/PDF/U2190.pdf  \u2193 == &darr;
+        //Toggle from down arrow to up arrow etc.
+        if ($( this ).html()=='\u2193'){
+            $( this ).html('&uarr;');
+        } else {
+            $( this ).html('&darr;');
+        }
     });
 });
+
