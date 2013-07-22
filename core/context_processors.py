1c081ecc0769388fffb36a386eaf5517e1bf630f
Chris McGinlay <ctmcginlay@gmail.com>
2013-07-22 19:26:10 +0100

    Fixes #72. Missed problem with path on staging server

--- core/context_processors.py
+++ core/context_processors.py
@@ -3,7 +3,7 @@ import subprocess
 def git_branch_render(request):
     """Obtain the active git branch and ensure it is present in template vars"""
     
-    process = subprocess.Popen("git branch |grep '*' |cut -c 2-", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    process = subprocess.Popen("cd /var/www/eduduck; git branch |grep '*' |cut -c 2-", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     agb = process.stdout.read()
 
     return {'ACTIVE_GIT_BRANCH': agb}
