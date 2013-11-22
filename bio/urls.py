7b73efad1aa6bec66ea88a8db5941f8ea3ed9617
chrismcginlay <ctmcginlay@gmail.com>
2013-11-22 23:56:19 +0000

    More work on password reset

--- bio/urls.py
+++ bio/urls.py
@@ -1,3 +1,4 @@
+from django.core.urlresolvers import reverse
 from django.conf.urls import patterns, url
 from django.conf import settings
 
@@ -5,4 +6,29 @@ urlpatterns = patterns ('bio.views',
     url(r'^$', 'bio'),
     url(r'^edit/$', 'edit'),
     url(r'^public/(?P<user_id>\d+)/$', 'public'),
+)
+
+urlpatterns += patterns( '',
+    url(r'^password_change/$', 'django.contrib.auth.views.password_change', { 
+        'template_name': 'bio/password_change_form.html',
+        'post_change_redirect': 'bio/password_change_done.html' }),
+    url(r'^password_change/done/$', 
+        'django.contrib.auth.views.password_change_done', { 
+        'template_name': 'bio/password_change_done.html', }),
+    url(r'^password_reset/$', 'django.contrib.auth.views.password_reset', {
+        'template_name': 'bio/password_reset_form.html',
+        'post_reset_redirect': 'bio/password_reset_done.html', }),
+    url(r'^password_reset_confirm/$', 
+        'django.contrib.auth.views.password_reset_confirm', {
+        'template_name': 'bio/password_reset_confirm', }),
+    url(r'^password_reset_done/$', 
+        'django.contrib.auth.views.password_reset_done', { 
+        'template_name': 'bio/password_reset_done.html',}),
+
+    # url(r'^password_reset/done/$', 'django.contrib.auth.views.password_reset_done', name='password_reset_done'),
+    # url(r'^reset/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    #     'django.contrib.auth.views.password_reset_confirm',
+    #     name='password_reset_confirm'),
+    # url(r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete', name='password_reset_complete'),
+
 )
--- bio/views.py
+++ bio/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import (render_to_response, get_object_or_404,
     
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
 
 from .forms import BioEditForm
 
