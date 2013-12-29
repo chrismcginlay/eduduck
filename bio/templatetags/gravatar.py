0963ae9535feefbd0fc8a469abe2d1f83eba2e32
chrismcginlay <ctmcginlay@gmail.com>
2013-12-29 18:48:20 +0000

    Added size parameter to gravatar_url tag, added gravatar_profile tag
    The gravatar profile tag links to the user's Gravatar.com account. The size parameter (required) is a pixel width for the avatar

--- bio/templatetags/gravatar.py
+++ bio/templatetags/gravatar.py
@@ -15,29 +15,56 @@ import urllib, hashlib
 register = template.Library()
  
 class GravatarUrlNode(template.Node):
+    def __init__(self, email, size):
+        self.email = template.Variable(email)
+        self.size = template.Variable(size)
+ 
+    def render(self, context):
+        try:
+            email = self.email.resolve(context)
+            size = self.size.resolve(context)
+        except template.VariableDoesNotExist:
+            return ''
+ 
+        default = "wavatar"
+ 
+        gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
+        gravatar_url += urllib.urlencode({'d':default, 's':str(size)})
+ 
+        return gravatar_url
+
+class GravatarProfileNode(template.Node):
+    """Create object linking to profile"""
+
     def __init__(self, email):
         self.email = template.Variable(email)
- 
+
     def render(self, context):
         try:
             email = self.email.resolve(context)
         except template.VariableDoesNotExist:
             return ''
  
-        default = "wavatar"
-        size = 40
+        gravatar_profile = "http://www.gravatar.com/" + hashlib.md5(email.lower()).hexdigest() 
  
-        gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
-        gravatar_url += urllib.urlencode({'d':default, 's':str(size)})
- 
-        return gravatar_url
+        return gravatar_profile
  
 @register.tag
 def gravatar_url(parser, token):
     try:
+        tag_name, email, size = token.split_contents()
+ 
+    except ValueError:
+        raise template.TemplateSyntaxError, "%r tag requires exactly two arguments" % token.contents.split()[0]
+ 
+    return GravatarUrlNode(email, size)
+
+@register.tag
+def gravatar_profile(parser, token):
+    try:
         tag_name, email = token.split_contents()
  
     except ValueError:
-        raise template.TemplateSyntaxError, "%r tag requires a single argument" % token.contents.split()[0]
+        raise template.TemplateSyntaxError, "%r tag requires one argument" % token.contents.split()[0]
  
-    return GravatarUrlNode(email)
+    return GravatarProfileNode(email)
