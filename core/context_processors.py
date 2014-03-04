4fcbccb8fd493e6701a66d60b15e55d9d2b181a9
Chris McGinlay <ctmcginlay@gmail.com>
2014-03-04 17:05:40 +0000

    Made branch diagnostic path use SITE_ROOT

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,4 +1,5 @@
 import subprocess
+from django.conf import settings
 from django.shortcuts import get_object_or_404, get_list_or_404
 from interaction.models import UserCourse, UserLesson
 from courses.models import Lesson
@@ -6,7 +7,13 @@ from courses.models import Lesson
 def git_branch_render(request):
     """Obtain the active git branch and ensure it is present in template vars"""
     
-    process = subprocess.Popen("cd /var/www/eduduck; git branch |grep '*' |cut -c 2-", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    cmd_str = "cd {0}; git branch |grep '*' |cut -c 2-".format(
+        settings.SITE_ROOT)
+    process = subprocess.Popen(cmd_str,
+                               shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT
+                               )
     agb = process.stdout.read()
 
     return {'ACTIVE_GIT_BRANCH': agb}
