4a6e34c46bc50e28cb173f98a607d46703c5b063
Chris McGinlay <ctmcginlay@gmail.com>
2014-03-13 16:45:51 +0000

    First half of draft fabfile

---
+++ deploy_tools/fabfile.py
@@ -0,0 +1,41 @@
+# Fabfile.py based on "Test Driven Web Development with Python, H. Percival, pp 144"
+from fabric.contrib.files import append, exists, sed
+from fabric.api import env, local, run
+import random
+
+REPO_URL = "https://github.com/chrismcginlay/eduduck.git"
+SITES_DIR = "/home/chris/sites"
+SOURCE_DIR = "{0}/{1}/source".format(SITES_DIR, env.host)
+
+def deploy():
+    _create_dir_tree_if_not_exists(env.host)
+    _get_source()
+    _update_settings(env.host)
+    _prepare_database()
+    _update_virtualenv()
+    _update_static()
+    _update_media()
+    
+def _create_dir_tree_if_not_exists(site_name):
+    for subdir in ("static", "media", "source", "virtualenv"):
+        run("mkdir -p {0}/{1}/{2}".format(SITES_DIR, site_name, subdir))
+        
+def _get_source():
+    #if git not at head, issue push warning and..
+    #local("cd {0}; git push").format(SOURCE_DIR)) 
+    if exists(SOURCE_DIR + '/.git'):
+        run("cd {0}; git fetch".format(SOURCE_DIR))
+    else:
+        run("git clone {0} {1}".format(REPO_URL, SOURCE_DIR))
+    current_commit = local("git log -n 1 --format=%H", capture = True)
+    run("cd {0}; git reset --hard {1}".format(SOURCE_DIR, current_commit))
+     
+def _update_settings(site_name):
+    #compare with how this is already done!
+    secret_key_file = #wherever it is to be
+        if not exists(secret_key_file):
+            charset = #see issue queue for key gen method used
+            key = #generate
+            append(secret_key_file, "SECRET_KEY={0}".format(key))
+        append??
+        
