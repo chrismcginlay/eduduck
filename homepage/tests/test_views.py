9220091f9b60a95e537ebca031388a550afaf175
chrismcginlay <ctmcginlay@gmail.com>
2014-06-05 00:33:39 +0100

    Enhancing hompage test coverage

--- homepage/tests/test_views.py
+++ homepage/tests/test_views.py
@@ -25,8 +25,24 @@ class HomepageViewTests(TestCase):
     def test_handle_no_courses_case(self):
         # Still renders template even if no courses
         
-        import pdb; pdb.set_trace()
         Course.objects.all().delete()
         response = self.client.get('/')
         self.assertInHTML('<p>No courses?</p>', response.content)
+    
+    def test_handle_just_one_course(self):
+        # Can render template if just one course
         
+        ##leave just one course
+        [c.delete() for c in Course.objects.all()[1:]]
+        response = self.client.get('/')
+        self.assertIn('div class="random_course pure-u-24-24">', 
+                      response.content)
+        
+    def test_handle_two_courses(self):
+        # Renders template with just two courses
+        
+        ##leave just two courses
+        [c.delete() for c in Course.objects.all()[2:]]
+        response = self.client.get('/')
+        self.assertIn('div class="random_course pure-u-', 
+                      response.content)       
