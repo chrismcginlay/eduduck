388271093bcc18790fdc57bad091166d18c1abc4
chrismcginlay <ctmcginlay@gmail.com>
2014-10-10 00:28:06 +0100

    Probably a correct file upload test, but doesn't pass yet

---
+++ attachment/utils.py
@@ -0,0 +1,13 @@
+#attachment/utils.py
+
+def generate_file(fname):
+    """Generate a dummy file, typically for upload testing
+
+    Concept from http://stackoverflow.com/a/11171303/1593443"""
+
+    gen_file = open(fname, 'a')
+    gen_file.write('Testing file upload?')
+    gen_file.write('Does it work?')
+
+    return gen_file
+
