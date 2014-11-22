3331ca97678750919ca9edd504b7c2e187e42faf
chrismcginlay <ctmcginlay@gmail.com>
2014-11-22 09:47:53 +0000

    Profile utilities and tests

---
+++ profile/tests/test_utils.py
@@ -0,0 +1,30 @@
+#profile/tests/test_utils.py
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from django.test import TestCase
+from social.backends.google import GoogleOAuth2#,FacebookOAuth2
+
+from ..utils import get_image_path, get_user_avatar
+
+class TestProfileUtilities(TestCase):
+
+    fixtures = [
+        'auth_user.json', 
+    ]
+ 
+    def test_get_user_avatar_for_facebook(self):
+        f = get_user_avatar
+        import pdb; pdb.set_trace()
+        self.fail("figure out, or abandon?")
+
+    def test_get_user_avatar_for_google_oauth2(self):
+        backend = GoogleOAuth2
+        user = get_object_or_404(User, id=1)
+        get_user_avatar(backend, user, response, *args, **kwargs)
+
+    def test_get_image_path(self):
+        person = User.objects.get(pk=1)
+        p = get_image_path(person, 'test.jpg')
+        self.assertEqual(p, "avatars/{0}/test.jpg".format(person.id))
+
+ 
---
+++ profile/utils.py
@@ -0,0 +1,45 @@
+# profile/utilities
+from django.contrib.auth.models import User
+from django.core.files.images import ImageFile
+from social.backends.facebook import FacebookOAuth2
+from social.backends.google import GoogleOAuth2
+
+def get_user_avatar(backend, user, response, *args, **kwargs):
+    import pdb; pdb.set_trace()
+    if isinstance(backend, GoogleOAuth2):
+        if response.get('image') and response['image'].get('url'):
+            url = response['image'].get('url')
+
+#???
+            ext = url.split('.')[-1]
+            user.profile.avatar.save(
+                '{0}{1}'.format('avatar', ext),
+                ImageFile(urllib2.urlopen(url).read()),
+                save=False
+            )
+            user.save()
+#???
+    if isinstance(backend, FacebookOAuth2):
+        if response.get('id'):
+            url = response('id').get('url')
+            
+    if isinstance(backend, Twitter):
+        if response.get('profile_image_url'):
+            url = response.get('profile_image_url', '').replace('_normal','')
+
+#use ImageFile as above
+    if url:
+        profile = user.profile()
+        avatar = urlopen(url).read()
+        fout = open(filepath, "wb")
+        fout.write(avatar)
+        fout.close()
+        profile.avatar = None #path to image
+        profile.save
+
+def get_image_path(user, filename):
+    """Construct and return a filepath for user avatar images"""
+
+    pass
+
+
