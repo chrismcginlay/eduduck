80b62b69bd502afee9162b8886a369f618415f7f
chrismcginlay <ctmcginlay@gmail.com>
2015-01-15 22:11:25 +0000

    Add functions for inserting tags into context variables

--- core/context_processors.py
+++ core/context_processors.py
@@ -9,15 +9,27 @@ def git_branch_render(request):
     
     cmd_str = "cd {0}; git branch |grep '*' |cut -c 2-".format(
         settings.BASE_DIR)
-    process = subprocess.Popen(cmd_str,
-                               shell=True,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.STDOUT
-                               )
+    process = subprocess.Popen(
+        cmd_str, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     agb = process.stdout.read()
-    agb = "fish"
 
     return {'ACTIVE_GIT_BRANCH': agb}
 
 def git_most_recent_tag(request):
-    return "mvp_0.1.0_rimmer-456-g22b45fc3"
+    """git describe tag like mvp_0.1.0_rimmer-456-g22b45fc3"""
+    cmd_str = "cd {0}; git describe".format(settings.BASE_DIR)
+    process = subprocess.Popen(
+        cmd_str, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    mr_t = process.stdout.read()
+
+    return {'MOST_RECENT_TAG': mr_t}
+
+def git_most_recent_deployed(request):
+    """git describe like DEPLOYED-2014-08-23/2028-147-gf4bf722"""
+    cmd_str = "cd {0}; git describe --match DEPLOYED* --tags".format(
+        settings.BASE_DIR)
+    process = subprocess.Popen(
+        cmd_str, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    mr_d = process.stdout.read()
+
+    return {'MOST_RECENT_DEPLOYED': mr_d}
--- core/tests/test_context_processors.py
+++ core/tests/test_context_processors.py
@@ -1,7 +1,10 @@
 #core/tests/test_context_processors.py
 
 from django.test import TestCase
-from ..context_processors import git_most_recent_tag
+from ..context_processors import (
+    git_most_recent_tag,
+    git_most_recent_deployed
+)
 
 class ContextProcessorsTests(TestCase):
     
@@ -10,7 +13,8 @@ class ContextProcessorsTests(TestCase):
  
     def test_git_most_recent_tag(self):
         """Require git describe tag like mvp_0.1.0_rimmer-456-g22b45fc3"""
-        mr_t = git_most_recent_tag(self.client.request)
+        context_var = git_most_recent_tag(self.client.request)
+        mr_t = context_var['MOST_RECENT_TAG']
         tag_regexp = "^[a-zA-Z]{3}_([0-9][.])+[0-9]"\
             "((_[a-z]+-[0-9]+-g[0-9a-fA-F]{7})|$)"
         self.assertGreater(len(mr_t), 9, "Tag seems too short")
@@ -18,7 +22,8 @@ class ContextProcessorsTests(TestCase):
            
     def test_git_most_recent_deployed(self):
         """Require git describe like DEPLOYED-2014-08-23/2028-147-gf4bf722"""
-        mr_d = git_most_recent_deployed(self.client.request)
-        tag_regext = "^DEPLOYED-20[0-9]{2}-[0-9]{2}-[0-9]{2}/[0-9]{4}-[0-9]+-g[0-9a-fA-F]$"
+        context_var = git_most_recent_deployed(self.client.request)
+        mr_d = context_var['MOST_RECENT_DEPLOYED']
+        tag_regexp = "^DEPLOYED-20[0-9]{2}-[0-9]{2}-[0-9]{2}/[0-9]{4}((-[0-9]+-g[0-9a-zA-Z]{7})|$)"
         self.assertGreater(len(mr_d), 24, "Tag seem too short")
         self.assertRegexpMatches(mr_d, tag_regexp, "Tag looks wrong")
