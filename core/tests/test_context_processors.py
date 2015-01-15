e671c7f2372f9594dd56a141bac643ade4fb9b40
chrismcginlay <ctmcginlay@gmail.com>
2015-01-15 21:53:19 +0000

    Test for DEPLOYED tag

--- core/tests/test_context_processors.py
+++ core/tests/test_context_processors.py
@@ -13,6 +13,12 @@ class ContextProcessorsTests(TestCase):
         mr_t = git_most_recent_tag(self.client.request)
         tag_regexp = "^[a-zA-Z]{3}_([0-9][.])+[0-9]"\
             "((_[a-z]+-[0-9]+-g[0-9a-fA-F]{7})|$)"
-        self.assertGreater(len(mr_t), 10, "Tag seems to short")
+        self.assertGreater(len(mr_t), 9, "Tag seems too short")
         self.assertRegexpMatches(mr_t, tag_regexp, "Tag looks wrong")
-            
+           
+    def test_git_most_recent_deployed(self):
+        """Require git describe like DEPLOYED-2014-08-23/2028-147-gf4bf722"""
+        mr_d = git_most_recent_deployed(self.client.request)
+        tag_regext = "^DEPLOYED-20[0-9]{2}-[0-9]{2}-[0-9]{2}/[0-9]{4}-[0-9]+-g[0-9a-fA-F]$"
+        self.assertGreater(len(mr_d), 24, "Tag seem too short")
+        self.assertRegexpMatches(mr_d, tag_regexp, "Tag looks wrong")
