3f8859b4dbbf3767757f4c9fa3dcfaa80be771a4
chrismcginlay <ctmcginlay@gmail.com>
2015-01-20 21:59:05 +0000

    Added test for base class, fail->pass

--- core/tests/test_eduduck_exceptions.py
+++ core/tests/test_eduduck_exceptions.py
@@ -4,5 +4,9 @@ from django.test import TestCase
 from ..eduduck_exceptions import CheckRepError
 
 class EduDuckExceptionTests(TestCase):
-    def test_checkrep_failure_raises(self):
+
+    def test_CheckRepError_inherits_from_Exception(self):
+        self.assertIn(CheckRepError, Exception.__subclasses__())
+
+    def test_CheckRepError_raises(self):
         self.fail("Write") 
