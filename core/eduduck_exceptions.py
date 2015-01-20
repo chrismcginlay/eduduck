899727474934ffe7615e53d0eb4e2764a518a9b2
chrismcginlay <ctmcginlay@gmail.com>
2015-01-20 22:08:48 +0000

    Remove pointless test - builtin functionality

---
+++ core/eduduck_exceptions.py
@@ -0,0 +1,4 @@
+#core/eduduck_exceptions.py
+
+class CheckRepError(Exception):
+    pass
--- core/tests/test_eduduck_exceptions.py
+++ core/tests/test_eduduck_exceptions.py
@@ -8,5 +8,3 @@ class EduDuckExceptionTests(TestCase):
     def test_CheckRepError_inherits_from_Exception(self):
         self.assertIn(CheckRepError, Exception.__subclasses__())
 
-    def test_CheckRepError_raises(self):
-        self.fail("Write") 
