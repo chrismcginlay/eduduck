d445274f75213a2fd047ab4ce7b0ccaaea94ca69
chrismcginlay <ctmcginlay@gmail.com>
2015-07-27 00:43:20 +0100

    Switch fabfile back to master

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -225,8 +225,8 @@ def _get_source(sdir):
     
     #Uncomment the following if you need to checkout and test a branch in staging.
     #run("cd {0}; git checkout NN-your_branch".format(sdir))
-    #run("cd {0}; git checkout master".format(sdir))    
-    run("cd {0}; git checkout 147-ssl".format(sdir))
+    run("cd {0}; git checkout master".format(sdir))    
+    #run("cd {0}; git checkout 147-ssl".format(sdir))
         
 def _config_nginx(site_name, sdir, settings):
     if settings=='dev':
