c0b188e3c20606e121747b1f790a95ac667918d7
chrismcginlay <ctmcginlay@gmail.com>
2015-09-06 13:23:18 +0100

    Migration for courses.models

---
+++ courses/migrations/0002_auto_20150906_1222.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('courses', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='course',
+            options={'permissions': (('study_course', 'User can enrol and study this course'),)},
+        ),
+    ]
